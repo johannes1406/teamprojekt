54102fa683ba2d402335bef16ee1c3c9b13389ed
johannesmietz <johannes.mietz@student.kit.edu>
2022-05-03 17:13:20 +0200

    Testcaone erste Schritte

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from flask import app
+from flask import Flask, request
+app = Flask(__name__)
 @app.route('/')  # this is the home page route
 def hello_world():  # this is the home page function that generates the page code
     return "Teamprojekt"
